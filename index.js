88744760f7162f23731a624b6e3ddc22b46d44da
RayeEThompson <rachael.elizabeth.thompson@gmail.com>
2020-08-09 18:24:54 -0500

    feat: :sparkles: grid
    add grid inside the world to start the maze

--- index.js
+++ index.js
@@ -1,35 +1,34 @@
-const {
-    Engine,
-    Render,
-    Runner,
-    World,
-    Bodies
-  } = Matter;
-  
-  //canvass dimensions
-  const width = 600;
-  const height = 600;
-  
-  const engine = Engine.create();
-  const { world } = engine;
-  const render = Render.create({
-    element: document.body,
-    engine: engine,
-    options: {
-      width,
-      height,
-    },
-  });
-  Render.run(render);
-  Runner.run(Runner.create(), engine);
+const { Engine, Render, Runner, World, Bodies } = Matter;
 
-  //Walls for canvass
-  const walls = [
-    Bodies.rectangle(width/2, 0, width, 40, { isStatic: true }),
-    Bodies.rectangle(width/2, height, width, 40, { isStatic: true }),
-    Bodies.rectangle(0, height/2, 40, height, { isStatic: true }),
-    Bodies.rectangle(width, height/2, 40, height, { isStatic: true }),
-  ];
-  World.add(world, walls);
-  
-  
+//canvass dimensions
+const width = 600;
+const height = 600;
+
+const engine = Engine.create();
+const { world } = engine;
+const render = Render.create({
+  element: document.body,
+  engine: engine,
+  options: {
+    width,
+    height,
+  },
+});
+Render.run(render);
+Runner.run(Runner.create(), engine);
+
+//Walls for canvass
+const walls = [
+  Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
+  Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
+  Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
+  Bodies.rectangle(width, height / 2, 40, height, { isStatic: true }),
+];
+World.add(world, walls);
+
+//Grid for maze (generator)ß
+const grid = Array(3)
+  .fill(null)
+  .map(() => Array(3).fill(false));
+
+console.log(grid);
