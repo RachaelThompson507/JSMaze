37a034f85ea9eb9e696135d21068a9b7b76b99a0
RayeEThompson <rachael.elizabeth.thompson@gmail.com>
2020-08-09 14:54:42 -0500

    feat(matterjs) implement world and walls for shapes

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
-const { Engine, Render, Runner, World, Bodies } = Matter;
-
-const engine= Engine.create();
-const world = engine;
-const render = Render.create({
+const {
+    Engine,
+    Render,
+    Runner,
+    World,
+    Bodies
+  } = Matter;
+  
+  //canvass dimensions
+  const width = 600;
+  const height = 600;
+  
+  const engine = Engine.create();
+  const { world } = engine;
+  const render = Render.create({
     element: document.body,
     engine: engine,
     options: {
-        width: 800,
-        height: 600
-    }
-});
-Render.run(render);
-Runner.run(Runner.create(), engine);
+      width,
+      height,
+    },
+  });
+  Render.run(render);
+  Runner.run(Runner.create(), engine);
 
-const shape = Bodies.rectangle(200, 200, 50, 50 {
-    isStatic: true
-});
-World.add(world, shape);
+  //Walls for canvass
+  const walls = [
+    Bodies.rectangle(width/2, 0, width, 40, { isStatic: true }),
+    Bodies.rectangle(width/2, height, width, 40, { isStatic: true }),
+    Bodies.rectangle(0, height/2, 40, height, { isStatic: true }),
+    Bodies.rectangle(width, height/2, 40, height, { isStatic: true }),
+  ];
+  World.add(world, walls);
+  
+  
